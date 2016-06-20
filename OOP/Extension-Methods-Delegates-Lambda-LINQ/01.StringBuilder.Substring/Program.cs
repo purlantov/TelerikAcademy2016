f7e422b87056d766e2864c8ccfb89c5a7be86280
Pavel <ppurlantov@yahoo.com>
2016-06-20 03:39:03 +0300

    extension methods

---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ/01.StringBuilder.Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.StringBuilder.Substring
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
