9bc632cda0384dc67eaa0dee0b9e0ef4126a06e9
purlantov <ppurlantov@yahoo.com>
2017-01-04 08:24:42 +0200

    OOP

--- OOP/02.DefiningClasses_Part2/Matrix_Generic/GenericList.cs
+++ OOP/02.DefiningClasses_Part2/Matrix_Generic/GenericList.cs
@@ -1,30 +1,82 @@
 ﻿using System;
 namespace Matrix_Generic
 {
-    public class GenericList<T> where T:struct
-    {
-        private const int DefaultCapacity = 16;
-        private int capacyty;
-        private T[] list;
+	public class GenericList<T>
+	{
+		private const int InitialCapacity = 4;
+		//private int capacity;
+		private int nextElement;
+		private T[] elements;
 
-        public GenericList()
-        {
-            this.list = new T[DefaultCapacity];
-        }
+		public int Count { get; private set; }
 
-        public T[] Grow()
-        {
-            this.list = new T[list.Length * 2];
+		public int Capacity
+		{
+			get { return elements.Length; }
+		}
 
-            return list;
-        }
+		public GenericList()
+		{
+			this.Count = 0;
+			this.elements = new T[InitialCapacity];
+		}
 
-        public void AddElement()
-        {
-            for (int i = 0; i < list.Length; i++)
-            {
-                
-            }
-        }
-    }
+		public void Add(T item)
+		{
+			// resize if necessary
+			if (this.Count == this.Capacity)
+			{
+				this.Grow();
+			}
+
+			elements[this.Count++] = item;
+		}
+
+		// indexer
+		public T this[int index]
+		{
+			get
+			{
+				return this.elements[index];
+			}
+			set
+			{
+				this.elements[index] = value;
+			}
+		}
+
+		public void RemoveElement(T item)
+		{
+
+		}
+
+		public void RemoveAt(int index)
+		{
+			for (int i = index; i < this.Count - 1; i++)
+			{
+				this.elements[i] = this.elements[i + 1];
+			}
+
+			this.elements[this.Count - 1] = default(T);
+
+			this.Count--;
+		}
+
+		private void Grow()
+		{
+			var newElements = new T[this.Capacity * 2];
+
+			for (int i = 0; i < this.Count; i++)
+			{
+				newElements[i] = this.elements[i];
+			}
+
+			this.elements = newElements;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine(string.Join(", ", this.elements));
+		}
+	}
 }
--- OOP/02.DefiningClasses_Part2/Matrix_Generic/Program.cs
+++ OOP/02.DefiningClasses_Part2/Matrix_Generic/Program.cs
@@ -7,6 +7,28 @@ namespace Matrix_Generic
         public static void Main(string[] args)
         {
             GenericList<double> lis = new GenericList<double>();
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(1);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			lis.Add(2);
+			
         }
     }
 }
